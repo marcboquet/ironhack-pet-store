a2e5a852dbbb3a78f926b2aa73909950b2d21958
Marc Boquet <marc@apalan.cat>
2014-10-09 14:44:22 +0200

    Scaffold for Pet and Species

---
+++ db/migrate/20141009124236_create_pets.rb
@@ -0,0 +1,10 @@
+class CreatePets < ActiveRecord::Migration
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.integer :species_id
+
+      t.timestamps
+    end
+  end
+end
