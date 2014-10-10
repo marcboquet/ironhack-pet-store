4a4ebe6873189cc49a66b488eb24790b0532a389
Marc Boquet <marc@apalan.cat>
2014-10-10 15:18:59 +0200

    Mailer and views for sending email

---
+++ app/mailers/notifications.rb
@@ -0,0 +1,25 @@
+class Notifications < ActionMailer::Base
+  default from: "no-reply@pet-a-porter.dev"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifications.signup.subject
+  #
+  def signup
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifications.bought_pet.subject
+  #
+  def bought_pet
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
