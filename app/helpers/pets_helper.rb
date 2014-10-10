c02812c8370e47d077d3e268d7c8247c6587001f
Marc Boquet <marc@apalan.cat>
2014-10-10 12:21:51 +0200

    Helper

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,2 +1,9 @@
 module PetsHelper
+  def show_me_the_image pet
+    if pet.photo.present?
+      image_tag(pet.photo)
+    else
+      "NO PHOTO"
+    end
+  end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,7 @@
 class Pet < ActiveRecord::Base
   belongs_to :species
   mount_uploader :photo, PetPhotoUploader
-  
+
   validates_presence_of :name, :species
+  
 end
