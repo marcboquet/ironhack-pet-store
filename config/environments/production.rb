8560e8baa79d0e2a32f014a0c2d9f84545725955
Marc Boquet <marc@apalan.cat>
2014-10-10 16:00:26 +0200

    send email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: 587,
+    domain: 'pet-a-porter.dev',
+    user_name: 'apalancat',
+    password: 'vj8XFc38hEVoOEkJGTiWBg',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
