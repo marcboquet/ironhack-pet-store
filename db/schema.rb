f57363c00fa09a1925859127fe2bd4c9ac1e4e69
Marc Boquet <marc@apalan.cat>
2014-10-10 14:44:10 +0200

    Add admin to user, check for admin in layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010103928) do
+ActiveRecord::Schema.define(version: 20141010123750) do
 
   create_table "pets", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20141010103928) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
